311f9bb3d30d71e8167f8a753f6245d9e41e776d
Carrie Olson-Manning <51338795+olso2066@users.noreply.github.com>
2025-03-21 08:54:53 -0500

    Update BINF_4_blast.sh

--- RNAseq/BINF_4_blast.sh
+++ RNAseq/BINF_4_blast.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=0:30:00
+#SBATCH --time=2:00:00
 #SBATCH --job-name=blast
 #SBATCH --output=blast_%a.out
 #SBATCH --error=blast_%a.err
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=4
 
-module load biocontainers ncbi-blast/2.16.0
+ml biocontainers blast/2.13.0
 
 # Only do these next steps once
 wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
@@ -15,6 +15,5 @@ gunzip uniprot_sprot.fasta.gz
 # Build a blasat data base
 makeblastdb -in uniprot_sprot.fasta -dbtype prot
 
-# do a blastx search 
+# do a blastx search
 blastx -query trinity_out_dir.Trinity.fasta -db uniprot_sprot.fasta -out blastx.outfmt6 -evalue 1e-20 -outfmt 0 
-
