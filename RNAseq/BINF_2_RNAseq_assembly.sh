a52188f65a9d7f276474273868fcdc20ae731ad6
Carrie Olson-Manning <51338795+olso2066@users.noreply.github.com>
2025-03-21 09:21:59 -0500

    Update BINF_2_RNAseq_assembly.sh

--- RNAseq/BINF_2_RNAseq_assembly.sh
+++ RNAseq/BINF_2_RNAseq_assembly.sh
@@ -5,12 +5,14 @@
 #SBATCH --error=assembly_%a.err
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=4
+
 # Load required modules
 ml biocontainers trinity/2.15.0 samtools
+
 # /anvil/projects/x-bio250083/olsonman_RNAseq
 Trinity --seqType fq \
---left /anvil/projects/x-bio250083/olsonman_RNAseq/out17.R1.fq.gz \
---right /anvil/projects/x-bio250083/olsonman_RNAseq/out17.R2.fq.gz \
+--left $SCRATCH/RNAseq/out.R1.fq.gz \
+--right $SCRATCH/RNAseq/out.R2.fq.gz \
 --CPU 6 \
 --max_memory 150G
 
