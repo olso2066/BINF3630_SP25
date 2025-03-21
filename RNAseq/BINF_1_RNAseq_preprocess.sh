e9dac6366c70a78939a518b7a52755ac494a2bcf
Carrie Olson-Manning <51338795+olso2066@users.noreply.github.com>
2025-03-21 14:25:02 -0500

    Update BINF_1_RNAseq_preprocess.sh

--- RNAseq/BINF_1_RNAseq_preprocess.sh
+++ RNAseq/BINF_1_RNAseq_preprocess.sh
@@ -22,23 +22,44 @@
 
 # import modules
 ml biocontainers fastp/0.23.2 fastqc/0.12.1
+ml sortmerna
 
 # Set up directories
-#### Change the ouput directory
 INPUT_DIR="/anvil/projects/x-bio250083/RNAseq_mdg"
+
+### ENTER YOUR SAMPLE NAME 
+SAMPLE_R1="$INPUT_DIR/mdg_017_L_S17_L004_R1_001.fastq.gz"
+SAMPLE_R2="$INPUT_DIR/mdg_017_L_S17_L004_R2_001.fastq.gz"
+
 OUTPUT_DIR="$SCRATCH/RNAseq"
 mkdir -p "$OUTPUT_DIR"
 
+cd "$OUTPUT_DIR"
+cp "$SAMPLE_R1" "$OUTPUT_DIR"
+cp "$SAMPLE_R2" "$OUTPUT_DIR"
+
 # Define sample names
 # change to your sample names ####
-SAMPLE_R1="$INPUT_DIR/mdg_017_L_S17_L004_R1_001.fastq.gz"
-SAMPLE_R2="$INPUT_DIR/mdg_017_L_S17_L004_R2_001.fastq.gz"
+# SAMPLE_R1="$INPUT_DIR/mdg_017_L_S17_L004_R1_001.fastq.gz"
+# SAMPLE_R2="$INPUT_DIR/mdg_017_L_S17_L004_R2_001.fastq.gz"
 
 # Step 1: Subsample and trim with fastp
 fastp -i "$SAMPLE_R1" -I "$SAMPLE_R2" \
       -o "$OUTPUT_DIR/out.R1.fq.gz" -O "$OUTPUT_DIR/out.R2.fq.gz" \
       --reads_to_process 10000 \ # we will only process a sub-sample for this RNAseq demonstration
       --thread 2
+
+
+# Step 3: Remove rRNA and mitochondrial reads with SortMeRNA
+SORTMERNA_OUT="$OUTPUT_DIR/sortmerna_out"
+mkdir -p "$SORTMERNA_OUT"
+sortmerna --ref smr_v4.3_fast_db.fasta \
+    --reads "$OUTPUT_DIR/out17.R1.fq.gz","$OUTPUT_DIR/out17.R2.fq.gz" \
+    --paired_in \
+    --workdir "$SORTMERNA_OUT" \
+    --fastx \
+    --other "$SORTMERNA_OUT/non_rrna" \
+    --aligned "$SORTMERNA_OUT/aligned"
 
 # Step 2: Run FastQC on trimmed reads
 fastqc "$OUTPUT_DIR/out.R1.fq.gz" "$OUTPUT_DIR/out.R2.fq.gz" \
