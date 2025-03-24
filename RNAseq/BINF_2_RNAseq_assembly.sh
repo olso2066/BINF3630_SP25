39b64b38daff230553c98ef339812e32bbe8cd2d
Carrie Olson-Manning <51338795+olso2066@users.noreply.github.com>
2025-03-24 11:40:54 -0500

    Update BINF_2_RNAseq_assembly.sh

--- RNAseq/BINF_2_RNAseq_assembly.sh
+++ RNAseq/BINF_2_RNAseq_assembly.sh
@@ -5,12 +5,14 @@
 #SBATCH --error=assembly_%a.err
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=4
+
 # Load required modules
 ml biocontainers trinity/2.15.0 samtools
+
 # Align trimmed reads
 Trinity --seqType fq \
 --left $SCRATCH/RNAseq/clean.R1.fq.gz \
 --right $SCRATCH/RNAseq/clean.R2.fq.gz \
 --CPU 6 \
 --max_memory 150G \
---output $SCRATCH/RNAseq/trinity_output
+
