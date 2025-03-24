3a70928c19990ecce41b134f773cf2c9472f7093
Carrie Olson-Manning <51338795+olso2066@users.noreply.github.com>
2025-03-24 09:27:52 -0500

    Update BINF_4_blast.sh

--- RNAseq/BINF_4_blast.sh
+++ RNAseq/BINF_4_blast.sh
@@ -4,11 +4,12 @@
 #SBATCH --output=blast_%a.out
 #SBATCH --error=blast_%a.err
 #SBATCH --mem=20G
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=10
 
-ml biocontainers blast/2.13.0
+ml biocontainers blast/2.13.0 
+ml seqkit/2.3.1
 
-# Only do these next steps once - if the blast fails, make sure to 
+# Only do these next steps once - if the blast fails, make sure to hashtag this out
 wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
 mv uniprot_sprot.fasta.gz $SCRATCH/RNAseq/
 gunzip $SCRATCH/RNAseq/uniprot_sprot.fasta.gz
@@ -16,5 +17,26 @@ gunzip $SCRATCH/RNAseq/uniprot_sprot.fasta.gz
 # Build a blasat data base
 makeblastdb -in $SCRATCH/RNAseq/uniprot_sprot.fasta -dbtype prot
 
+mkdir split_fasta
+cd split_fasta
+seqkit split -p 10 ../trinity_output/Trinity.fasta
+
+# another way
+# csplit -z -f chunk_ -n 2 ../trinity_output/Trinity.fasta '/^>/' '{*}'
+
+mkdir ../blast_results
+parallel -j 10 'blastx -query {} -db ../uniprot_sprot.fasta \
+         -evalue 1e-20 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore slen" \
+         -num_threads 1 -out ../blast_results/{/.}.blastx' ::: *.fasta
+
+cat ../blast_results/*.blastx > blastx_combined.outfmt6
+
+
 # do a blastx search
-blastx -query $SCRATCH/RNAseq/trinity_out_dir.Trinity.fasta -db $SCRATCH/RNAseq/uniprot_sprot.fasta -out $SCRATCH/RNAseq/blastx.outfmt6 -evalue 1e-20 -outfmt 0 
+# blastx -query $SCRATCH/RNAseq/trinity_out_dir.Trinity.fasta -db $SCRATCH/RNAseq/uniprot_sprot.fasta -out $SCRATCH/RNAseq/blastx.outfmt6 -evalue 1e-20 -outfmt 0 
+# blastx -query trinity_output/Trinity.fasta \
+#        -db uniprot_sprot.fasta \
+#        -out blastx.outfmt6 \
+#        -evalue 1e-20 \
+#        -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore slen" \
+#        -num_threads 10
